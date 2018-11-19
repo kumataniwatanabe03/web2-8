819e56bed60cc08f91e44e380958a0866dd74334
kumataniwatanabe03 <38421097+kumataniwatanabe03@users.noreply.github.com>
2018-11-19 18:41:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -5,13 +5,22 @@ var separate_time = function(time){
   var days  = Math.floor( time / 1000 / 60 / 60 / 24);
   return [sec, min, hours, days];}
 
-var now = new Date();
-var target = new Date(2018,12,7,24,0,0,0,0);
-var diff = target.getTime() - now.getTime();
-var counter = separate_time(diff);
-document.getElementById('countdown').textContent =
+var update = function(){
+ var now = new Date();
+ var target = new Date(2018,12,7,24,0,0,0,0);
+ var diff = target.getTime() - now.getTime();
+ var counter = separate_time(diff);
+ document.getElementById('countdown').textContent =
    'スマブラ発売まであと '
    +counter[3] + '日'
    +counter[2] + '時間'
    +counter[1] + '分'
    +counter[0] + '秒';
+ refresh(); //タイマーを起動 }
+
+var refresh= function(){
+  setTimeout(update, 1000); //1000ミリ秒待ってからupdateを実行 }
+
+update(); //最初の更新
+
+ 

fd290ba71879b84d2af60776b22ec080bb135038
kumataniwatanabe03 <38421097+kumataniwatanabe03@users.noreply.github.com>
2018-11-19 17:54:28 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,9 +1,10 @@
 var separate_time = function(time){
-  var sec   = Math.floor((time / 1000) % 60);
-  var min   = Math.floor((time / 1000 / 60) % 60);
-  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
-  var days  = Math.floor(time / 1000 / 60 / 60 / 24);
-  return [sec, min, hours, days];}
+  var sec    = time.getSeconds();
+  var min    = time.getMinutes();
+  var hours  = time.getHours();
+  var days   = time.getDate();
+return [sec, min, hours, days,];}
+
 
 var now = new Date();
 var counter = separate_time(diff);

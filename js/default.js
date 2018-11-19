240bf7d375c7a1ad153c290576a664ef7bdd173f
kumataniwatanabe03 <38421097+kumataniwatanabe03@users.noreply.github.com>
2018-11-19 18:33:33 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,16 +3,14 @@ var separate_time = function(time){
   var min   = Math.floor((time / 1000 / 60) % 60);
   var hours = Math.floor((time / 1000 / 60 / 60) % 24);
   var days  = Math.floor( time / 1000 / 60 / 60 / 24);
-  var month = Math.floor( time / 1000 / 60 / 60 / 60/ 24);
   return [sec, min, hours, days];}
 
 var now = new Date();
-var target = new Date(2018,12,7,0,0,0,0);
+var target = new Date(2018,12,7,24,0,0,0,0);
 var diff = target.getTime() - now.getTime();
 var counter = separate_time(diff);
 document.getElementById('countdown').textContent =
    'スマブラ発売まであと '
-   +counter[4] + '月'
    +counter[3] + '日'
    +counter[2] + '時間'
    +counter[1] + '分'

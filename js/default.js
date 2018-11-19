cf70c591597d3828549edaf3609a3c4df0a47c2b
kumataniwatanabe03 <38421097+kumataniwatanabe03@users.noreply.github.com>
2018-11-19 18:17:41 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,11 +1,12 @@
 var separate_time = function(time){
-  var sec    = time.getSeconds();
-  var min    = time.getMinutes();
-  var hours  = time.getHours();
-  var days   = time.getDate();
-  return [sec, min, hours, days]; }
+  var sec   = Math.floor((time / 1000) % 60);
+  var min   = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+  var days  = Math.floor(time / 1000 / 60 / 60 / 24);
+  return [sec, min, hours, days];}
 
 var now = new Date();
+var target = new Date(2018,12,7,0,0,0,0);
 var counter = separate_time(now);
 document.getElementById('countdown').textContent =
    counter[3] + '日' +

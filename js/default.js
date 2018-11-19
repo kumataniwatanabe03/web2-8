8f565cfd3397cdb5a1a78f724bfda363fc3bde98
kumataniwatanabe03 <38421097+kumataniwatanabe03@users.noreply.github.com>
2018-11-19 17:48:15 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,11 +1,9 @@
 var separate_time = function(time){
-  var sec    = time.getSeconds();
-  var min    = time.getMinutes();
-  var hours  = time.getHours();
-  var days   = time.getDate();
-  var month  = time.getMonth();
-  var year   = time.FullYear();
-return [sec, min, hours, days, month, year];}
+  var sec   = Math.floor((time / 1000) % 60);
+  var min   = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+  var days  = Math.floor(time / 1000 / 60 / 60 / 24);
+  return [sec, min, hours, days];}
 
 var update = function(){
   var now = new Date();
@@ -18,9 +16,3 @@ var update = function(){
     counter[2] + '時間' +
     counter[1] + '分' +
     counter[0] + '秒';
-  refresh();// タイマーを起動 }
- 
-var refresh= function(){
-  setTimeout(update, 1000); //1000ミリ秒待ってからupdateを実行 }
-
-update(); //最初の更新

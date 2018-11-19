5a237828e162337240ed8d40b56f708f983d37ef
kumataniwatanabe03 <38421097+kumataniwatanabe03@users.noreply.github.com>
2018-11-19 18:30:50 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,7 +2,8 @@ var separate_time = function(time){
   var sec   = Math.floor((time / 1000) % 60);
   var min   = Math.floor((time / 1000 / 60) % 60);
   var hours = Math.floor((time / 1000 / 60 / 60) % 24);
-  var days  = Math.floor( time / 1000 / 60 / 60 / % 24);
+  var days  = Math.floor( time / 1000 / 60 / 60 / 24);
+  var month = Math.floor( time / 1000 / 60 / 60 / 60/ 24);
   return [sec, min, hours, days];}
 
 var now = new Date();
@@ -10,7 +11,8 @@ var target = new Date(2018,12,7,0,0,0,0);
 var diff = target.getTime() - now.getTime();
 var counter = separate_time(diff);
 document.getElementById('countdown').textContent =
-   'スマブラ発売まであと ' 
+   'スマブラ発売まであと '
+   +counter[4] + '月'
    +counter[3] + '日'
    +counter[2] + '時間'
    +counter[1] + '分'
